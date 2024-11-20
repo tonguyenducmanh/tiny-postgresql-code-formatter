2248f5e84ea073cbc6bb11842a503d8aada76d4d
to nguyen duc manh <ducmanh14032000@gmail.com>
2024-11-20 11:55:46 +0700

    add tokenizer function

--- src/enumeration.js
+++ src/enumeration.js
@@ -1,2 +1,19 @@
 // object lưu enum dùng chung cho toàn bộ tool
-export const enumeration = {};
+export const enumeration = {
+  tokenType: {
+    // dấu đóng mở ngoặc
+    parenthesis: "parenthesis",
+    // từ khóa
+    keyword: "keyword",
+    // dấu chấm phẩy ;
+    semicolon: "semicolon",
+    // bình luận trong code
+    comment: "comment",
+    // xuống dòng
+    newLine: "newLine",
+    // số vd 123
+    number: "number",
+    // chữ cái vd "123"
+    text: "text",
+  },
+};
