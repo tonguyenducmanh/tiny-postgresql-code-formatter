c43eb67f3b4424a0fb53e072b34b327d32ec1ddb
to nguyen duc manh <ducmanh14032000@gmail.com>
2024-11-20 10:39:59 +0700

    tạm thời xóa file thừa

--- src/formatCode.js
+++ src/formatCode.js
@@ -2,25 +2,11 @@
  * file thực hiện nghiệp vụ format code
  */
 
-import { postgreSQLLanguage } from "./postgreSQLLanguage.js";
-
 /**
  * Hàm chính để format code
  * @param {string} sourceCode: text cần format
  * @returns
  */
 export function formatCode(sourceCode) {
-  if (!validateConfigPostgreSQL()) {
-    return sourceCode;
-  }
-}
-
-function validateConfigPostgreSQL() {
-  if (!postgreSQLLanguage.keyword) {
-    throw new Error("Not config PostGreSQL KeyWorld");
-  }
-  if (!postgreSQLLanguage.character) {
-    throw new Error("Not config PostGreSQL Character");
-  }
-  return true;
+  return sourceCode;
 }
--- src/postgreSQLLanguage.js
+++
@@ -1,7 +0,0 @@
-/**
- * object lưu toàn bộ các key world, ký tự được dùng trong ngôn ngữ postgreSQl được support bởi tool này
- */
-export const postgreSQLLanguage = {
-  keyword: ["select", "from", "where"],
-  character: ["=", ",", ">", "<", ";", "--", "/*", "*/"],
-};
